3a1aaf456d82e6be475da28ae81348fc3b114f62
Allen J Price <allen@starcity.com>
2018-04-06 10:10:42 -0700

    add longest run discussion

--- src/toy_problems/longest-run/longest_run.clj
+++ src/toy_problems/longest-run/longest_run.clj
@@ -1,1 +1,16 @@
 (ns toy-problems.longest-run.longest-run)
+
+
+
+
+
+(map-indexed (fn [i char]
+               [i, char]) "annyeong")
+
+(longest-run
+ [str]
+ (reduce
+  (fn [char]
+    )
+  {} ;; indices and empty characters
+  str))
