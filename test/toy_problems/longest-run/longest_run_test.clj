3d525eadab135fcedd1f6a5f8256025e8ffd0179
Allen J Price <allen@starcity.com>
2018-04-08 22:31:49 -0700

    refactor longest-run tests

--- test/toy_problems/longest-run/longest_run_test.clj
+++ test/toy_problems/longest-run/longest_run_test.clj
@@ -3,26 +3,18 @@
             [clojure.test :refer :all]))
 
 
-(deftest simple
+(deftest longest-run-test
   (testing "handles a simple case"
-    (is (= (longest-run "bobby") [2 3]))))
+    (is (= (longest-run "bobby") [2 3])))
 
-
-(deftest no-run
   (testing "returns [0 0] when there are no runs"
-    (is (= (longest-run "a dingo ate my baby") [0 0]))))
+    (is (= (longest-run "a dingo ate my baby") [0 0])))
 
-
-(deftest multiple-runs
   (testing "can handle inputs with multiple runs"
-    (is (= (longest-run "aabbbcc") [2 4]))))
+    (is (= (longest-run "aabbbcc") [2 4])))
 
-
-(deftest multiple-runs-of-same-length
   (testing "gives the first occurence of the longest run"
-    (is (= (longest-run "abbbccdddeee") [1 3]))))
+    (is (= (longest-run "abbbccdddeee") [1 3])))
 
-
-(deftest empty-input
   (testing "doesn't barf at an empty input"
     (is (= (longest-run "") [0 0]))))
