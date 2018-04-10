ba167af0fed4cbbeebfecfcb5bcb61d83d34edc4
Andres Pineda <humb.amp123@gmail.com>
2018-04-09 23:07:44 -0700

    add is-anagram?

--- src/toy_problems/is-anagram/is_anagram.clj
+++ src/toy_problems/is-anagram/is_anagram.clj
@@ -1,6 +1,19 @@
-(ns toy-problems.is-anagram.is-anagram)
+(ns toy-problems.is-anagram.is-anagram
+  (:require [clojure.string :as str]))
+
+(defn- remove-at
+  [v i]
+  (concat (subvec v 0 i) (subvec v (inc i))))
+
 
 (defn is-anagram?
   [str1 str2]
-  ;;TODO - your code here!
-  nil)
+  (boolean
+   (when (and (= (count str1) (count str2))
+              (not= str1 str2))
+     (empty? (reduce (fn [left char]
+                          (if-let [char-index (str/index-of left char)]
+                            (str/join "" (remove-at (str/split left #"") char-index))
+                            (reduced "not an anagram...")))
+                        str1
+                        str2)))))
