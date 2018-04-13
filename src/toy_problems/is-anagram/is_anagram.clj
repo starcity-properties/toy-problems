39cd71864bd48e08a58fa867d70afd02d7370a13
Queenie Ho <kuini.ho@gmail.com>
2018-04-13 09:24:22 -0700

    queenies is anagram

--- src/toy_problems/is-anagram/is_anagram.clj
+++ src/toy_problems/is-anagram/is_anagram.clj
@@ -1,6 +1,26 @@
 (ns toy-problems.is-anagram.is-anagram)
 
+
+(defn has-same-chars?
+  [str1 str2]
+  (= (set str1) (set str2)))
+
+
+(defn char-count
+  "given a char `c` return the amount of times it's in a string"
+  [c str]
+  (count (filter #(= c %) str)))
+
+
 (defn is-anagram?
   [str1 str2]
   ;;TODO - your code here!
-  nil)
+  (if (has-same-chars? str1 str2)
+    (reduce
+     (fn [acc c]
+       (if (= (char-count c str1) (char-count c str2))
+         true
+         (reduced false)))
+     true
+     (set str1))
+    false))
