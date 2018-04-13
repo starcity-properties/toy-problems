b14534a314bfa36a7388e8d9ab0c9fc41af7bfea
therese <tdiede@gmail.com>
2018-04-13 09:31:16 -0700

    terrible solution; pseudocode of better solution

--- src/toy_problems/is-anagram/is_anagram.clj
+++ src/toy_problems/is-anagram/is_anagram.clj
@@ -1,6 +1,30 @@
 (ns toy-problems.is-anagram.is-anagram)
 
+
+;; time: O(n log(n))
+;; space: O(n)
 (defn is-anagram?
   [str1 str2]
-  ;;TODO - your code here!
-  nil)
+  (and (not= str1 str2)
+       (= (sort str1) (sort str2))))
+
+
+(comment
+
+  (is-anagram? "abb" "bba")
+
+  (is-anagram? "aba" "bab")
+
+  (is-anagram? "abbr" "rbab")
+
+  (is-anagram? "abbc" "bbaa")
+
+  (is-anagram? "abc" "abc")
+
+  )
+
+
+;; time O(n)
+;; space O(1)
+;; constant space achieved by building a map of character frequencies...
+;; linear time achieved by iterating over first string to build map, then count down until frequencies are 0
