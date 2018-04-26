fe8590898c35739407438def5b3614d1891eadca
therese <tdiede@gmail.com>
2018-04-25 20:47:27 -0700

    solution

--- src/toy_problems/contiguous-sum/contiguous_sum.clj
+++ src/toy_problems/contiguous-sum/contiguous_sum.clj
@@ -1,6 +1,27 @@
 (ns toy-problems.contiguous-sum.contiguous-sum)
 
+;; Write a function that accepts a vector of numbers and returns the greatest contiguous sum of numbers in it.
+;; If a vector contains only one number, that single number will count as a contiguous sum.
+
+
 (defn contiguous-sum
   [numbers]
-  ;; TODO - your code here!
-  nil)
+  (first
+   (reduce
+    (fn [[max-sum running-sum] n]
+      (let [current-running-sum (max n (+ running-sum n))]
+        [(max max-sum current-running-sum) current-running-sum]))
+    [0 0] numbers)))
+
+
+(contiguous-sum [1 2 3]) ;;=> 6
+
+(contiguous-sum [1 2 3 -4]) ;;=> 6
+
+(contiguous-sum [1 2 3 -4 5]) ;;=> 7
+
+(contiguous-sum [4 -1 5]) ;;=> 8
+
+(contiguous-sum [10 -11 11]) ;;=> 11
+
+(contiguous-sum [-10 -11 30]) ;;=> 30
