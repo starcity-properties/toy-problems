89afd21901d06511ec384d0363ce249e35a63fd0
therese <tdiede@gmail.com>
2018-04-30 17:46:51 -0700

    use anon function reduce over first string to check set intersection

--- src/toy_problems/common-characters/common_characters.clj
+++ src/toy_problems/common-characters/common_characters.clj
@@ -1,6 +1,22 @@
-(ns toy-problems.common-characters.common-characters)
+(ns toy-problems.common-characters.common-characters
+  (:require [clojure.set :as cset]
+            [clojure.string :as cstring]))
+
+
+(defn- produce-common-set
+  [strings]
+  (->> (map set strings)
+       (apply cset/intersection)))
+
 
 (defn common-characters
-  [string1 string2]
-  ;;TODO - your code here!
-  nil)
+  [& strings]
+  (let [common-set (produce-common-set strings)]
+    (reduce #(if (and (contains? common-set %2)
+                      (not (cstring/includes? %1 (str %2))))
+               (str %1 %2)
+               %1)
+            "" (first strings))))
+
+
+(common-characters "lkaaaadaaasaaaaadjf" "aads" "lskdajfslls") ;;=> "ads"
