befbbe349dd462c6c2bd29c6c7e3e6a76e10197e
therese <tdiede@gmail.com>
2018-05-15 20:31:13 -0700

    solve balance parens

--- src/toy_problems/balanced-parens/balanced_parens.clj
+++ src/toy_problems/balanced-parens/balanced_parens.clj
@@ -1,6 +1,43 @@
 (ns toy-problems.balanced-parens.balanced-parens)
 
+
 (defn balanced-parens
-  [str]
-  ;;TODO - your code here!
-  nil)
+  [string]
+  (empty?
+   (reduce (fn [stack ch]
+             (let [standard-of-balance {\) \( \} \{ \] \[}
+                   in-close-parens?    (partial get standard-of-balance)
+                   in-open-parens?     (partial get (clojure.set/map-invert standard-of-balance))]
+
+               (cond
+                 (and (empty? stack)
+                      (some? (in-close-parens? ch)))
+                 (reduced (conj stack "exit early"))
+
+                 (and (not (empty? stack))
+                      (= (in-close-parens? ch) (peek stack)))
+                 (pop stack)
+
+                 (some? (in-open-parens? ch))
+                 (conj stack ch)
+
+                 :else stack)))
+           []
+           string)))
+
+
+(comment
+
+  (balanced-parens "()") ;; => true
+
+  (balanced-parens ")(") ;; => false
+
+  (balanced-parens "[]{}()") ;; => true
+
+  (balanced-parens "(lol [0 1 2 3] + {})") ;; => true
+
+  (balanced-parens "{:a 3 :b [{:c 4} {:d 5}] :e (inc 4)}") ;; => true
+
+  (balanced-parens "{:a 3 } :b [{:c 4} {:d 5}] :e (inc 4)}") ;; => false
+
+  )
