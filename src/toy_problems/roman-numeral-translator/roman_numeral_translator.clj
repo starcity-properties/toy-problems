fb91ce8418349e37a72f259b8666e3f187dd39bf
Cecilia Wanjiku <cecilia.wahome@andela.com>
2018-06-08 18:10:44 +0300

    roman-numerals

--- src/toy_problems/roman-numeral-translator/roman_numeral_translator.clj
+++ src/toy_problems/roman-numeral-translator/roman_numeral_translator.clj
@@ -1,17 +1,37 @@
 (ns toy-problems.path-parser.roman-numeral-translator.roman-numeral-translator)
 
 
-(def digit-values
-  {:I 1
-   :V 5
-   :X 10
-   :L 50
-   :C 100
-   :D 500
-   :M 1000})
+(ns roman-arabic-conv.core
+  (:gen-class))
 
+(def digit-values {:I 1
+                   :V 5
+                   :X 10
+                   :L 50
+                   :C 100
+                   :D 500
+                   :M 1000})
 
-(defn roman->arabic
-  []
-  ;;TODO - your code here!
-  nil)
+(defn char->keyword [c] (-> c str keyword))
+
+(defn compute-value [n1 n2]
+  (let [v1 (n1 digit-values)
+        v2 (n2 digit-values)]
+    (prn :v1 v1 :v2 v2)
+    (if (>= v1 v2)
+      (+ v1 v2)
+      (- v2 v1))))
+
+(defn roman->arabic [num arabic-sum]
+  (prn :num num :empty? (empty? num))
+  (if (empty? num)
+    arabic-sum
+    (let [first-num (char->keyword (first num))
+          second-num (char->keyword (second num))
+          rest-num (rest (rest num))]
+      (recur rest-num (+ arabic-sum (compute-value first-num second-num))))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (roman->arabic "MM" 0)))
